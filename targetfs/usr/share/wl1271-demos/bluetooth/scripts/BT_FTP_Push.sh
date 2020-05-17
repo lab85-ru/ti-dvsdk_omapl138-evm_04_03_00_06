603f0a130c36f58711f0fd3df2729a34f589a8d2
lab85.ru <info@lab85.ru>
2020-05-17 20:35:00 +0300

    Add ROOT targetfs.

---
+++ targetfs/usr/share/wl1271-demos/bluetooth/scripts/BT_FTP_Push.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+#/***************************************************************************
+# * \file    BT_FTP_Push.sh
+# *
+# * \brief   Script to demonstarte FTP over BT
+# *
+# * \author  Sinoj@Mistral
+# *
+# * \version 02a,28Jul10, modified
+# * \version 02b,09Dec10, modified
+# ***************************************************************************/
+
+echo "
+/*
+ * ########################## BT-FTP ################################
+ * File Transfer Profile (FTP) provides capability to transfer files
+ * and folders in an object store (file system) of another system.
+ * ##################################################################
+ */
+"
+echo "FTP from OMAP3EVM to remote device"
+echo "
+Note: 
+    For this test ensure that we have BD_ADDR of remote device and CH_NUM of
+    this remote device for FTP service
+"
+
+echo -n "Enter the BD_ADDR of the remote Device for FTP test
+====> "
+read BD_ADDR
+
+echo -n "Enter the channel number of FTP service 
+====> "
+read CH_NUM
+
+FTP_FILE_PATH=$GALLERY
+
+echo ""                                                                      
+echo "Local File Available under \"$FTP_FILE_PATH\""                                               
+echo ""                                                                       
+ls $FTP_FILE_PATH                                                             
+echo ""                                                                       
+                                                                              
+echo -n "Enter the local file name to be sent over FTP
+====> "                                                                       
+read LFILE                                                                   
+                                                                             
+                                                                             
+                                                                             
+ls $FTP_FILE_PATH/$LFILE &> /dev/null                                        
+if [ "$?" -ne 0 ]; then                                                      
+    obexftp -b $BD_ADDR -B $CH_NUM -p $LFILE
+else                                                                         
+    obexftp -b $BD_ADDR -B $CH_NUM -p $FTP_FILE_PATH/$LFILE
+fi  
